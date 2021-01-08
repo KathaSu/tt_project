94c3bfdb36b23bf7e0a5a515eab8e9d2fb80acf7
Katharina Suhr <suhr.katharina@gmail.com>
2021-01-08 15:30:27 +0100

    adding database of fanfictions

---
+++ create_database.py
@@ -0,0 +1,66 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+        conn = None
+        try:
+                conn = sqlite3.connect(db_file)
+                print(sqlite3.version)
+        except Error as e:
+                print(e)
+        return conn
+
+def create_table(conn, create_table_sql):
+        try:
+                c = conn.cursor()
+                c.execute(create_table_sql)
+        except Error as e:
+                print(e)
+
+
+def main():
+        database = r"fanfiction.db"
+
+        sql_create_tables_author = """CREATE TABLE IF NOT EXISTS author (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                name varchar(255) UNIQUE
+                                );"""
+
+        sql_create_tables_agerating = """CREATE TABLE IF NOT EXISTS age_rating (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                rating varchar(255) UNIQUE
+                                );"""
+
+        sql_create_tables_language = """CREATE TABLE IF NOT EXISTS language (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                language varchar(255) UNIQUE
+                                );"""
+
+        sql_create_tables_fanfiction = """CREATE TABLE IF NOT EXISTS fanfiction (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                title varchar(255),
+                                author_id INTEGER,
+                                age_rating_id INTEGER,
+                                tags varchar(255),
+                                characters varchar(255),
+                                language_id INTEGER,
+                                body text,
+                                FOREIGN KEY (author_id) REFERENCES author (id),
+                                FOREIGN KEY (age_rating_id) REFERENCES age_rating (id),
+                                FOREIGN KEY (language_id) REFERENCES language (id)
+                                ); """
+                                
+        conn = create_connection(database)
+
+        if conn is not None:
+                create_table(conn, sql_create_tables_author)
+                create_table(conn, sql_create_tables_agerating)
+                create_table(conn, sql_create_tables_language)
+                create_table(conn, sql_create_tables_fanfiction)
+        else:
+                print("Error cannot crate the database connection.")
+
+if __name__ == '__main__':
+        main()
+        
---
+++ fanfiction_to_database.py
@@ -0,0 +1,76 @@
+import pandas
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+    return conn
+
+def insert_author(conn, author):
+    Sql_insert_author = """INSERT OR IGNORE INTO author (name) VALUES ('{}')""".format(author)
+    Sql_select_author = """SELECT id FROM author WHERE name = '{}'""".format(author)
+    cur = conn.cursor()
+    cur.execute(Sql_insert_author)
+    author = cur.execute(Sql_select_author)
+    author_id = author.fetchall()
+    conn.commit()
+    return author_id[0][0]
+
+def insert_agerating(conn, age_rating):
+    Sql_insert_agerating = """INSERT OR IGNORE INTO age_rating (rating) VALUES ('{}')""".format(age_rating)
+    Sql_select_agerating = """SELECT id FROM age_rating WHERE rating = '{}'""".format(age_rating)
+    cur = conn.cursor()
+    cur.execute(Sql_insert_agerating)
+    agerating = cur.execute(Sql_select_agerating)
+    agerating_id = agerating.fetchall()
+    conn.commit()
+    return agerating_id[0][0]
+
+def insert_language(conn, language):
+    Sql_insert_language = """INSERT OR IGNORE INTO language (language) VALUES ('{}')""".format(language)
+    Sql_select_language = """SELECT id FROM language WHERE language = '{}'""".format(language)
+    cur = conn.cursor()
+    cur.execute(Sql_insert_language)
+    language = cur.execute(Sql_select_language)
+    language_id = language.fetchall()
+    conn.commit()
+    return language_id[0][0]
+
+def insert_fanfiction(conn, title, author, age_rating, tags, characters, language, body):
+    Sql_insert_fanfiction = """INSERT INTO fanfiction (title, author_id, age_rating_id, tags, characters, language_id, body) 
+                                VALUES("{}", {}, {}, "{}", "{}", {}, "{}") """.format(title, author, age_rating, tags, characters, language, body)
+    cur = conn.cursor()
+    cur.execute(Sql_insert_fanfiction)
+    conn.commit()
+
+def main():
+    database = r"fanfiction.db"
+    conn = create_connection(database)
+    data = pandas.read_csv(
+        'fanfics_1.csv',
+        delimiter=',',
+        encoding="latin-1",
+        converters={
+            "title": lambda x: x.replace('"', "'"),
+            "author": lambda x: x.strip("[]").replace("'", "").split(", "),
+            "character": lambda x: x.replace('"', "'"),
+            "additional tags": lambda x: x.replace('"', "'"),
+            "body": lambda x: x.replace("\r\n", " ").replace('"', "'")})
+    dataframe = pandas.DataFrame(data)
+
+    for index, row in dataframe.iterrows():
+        if row['language'] == "English":
+            print(row['work_id'])
+            with conn:
+                author = insert_author(conn, row["author"][0])
+                age_rating = insert_agerating(conn, row["rating"])
+                langauge = insert_language(conn, row["language"])
+                insert_fanfiction(conn, row["title"], author, age_rating, row["additional tags"], row["character"], langauge, row["body"])
+
+if __name__ == '__main__':
+    main()
